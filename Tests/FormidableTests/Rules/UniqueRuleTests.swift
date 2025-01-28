5f94407a88d15f990a59ddb1603305d9d8441633
Adriano Souza Costa <adrianosouzacostaios@gmail.com>
2025-01-28 14:00:18 -0300

    Improving UniqueRule tests

--- Tests/FormidableTests/Rules/UniqueRuleTests.swift
+++ Tests/FormidableTests/Rules/UniqueRuleTests.swift
@@ -19,17 +19,67 @@ struct UniqueRuleTests {
         try rule.validate(nil)
     }
     
-    @Test func mustBeValidWhenValueIsUnique() throws {
+    @Test func mustBeValidWhenValueNotExistsYet() throws {
         let rule = UniqueRule<Any, Int>(in: [10], error: error)
         
         try rule.validate(11)
     }
     
-    @Test func mustBeInvalidWhenValueIsNotUnique() throws {
+    @Test func mustBeInvalidWhenValueAlreadyExists() throws {
         let rule = UniqueRule<Any, Int>(in: [10, 11, 12], error: error)
         
         #expect(throws: error) {
             try rule.validate(11)
+        }
+    }
+    
+    @Test func mustBeValidWhenKeyPathValueNotExistsYet() throws {
+        let game = Game([1, 2, 3, 4])
+        let rule = UniqueRule(
+            game,
+            keyPath: \.values,
+            error: error
+        )
+        
+        try rule.validate(11)
+    }
+    
+    @Test func mustBeInvalidWhenKeyPathValueAlreadyExists() throws {
+        let game = Game([1, 2, 3, 4])
+        let rule = UniqueRule(
+            game,
+            keyPath: \.values,
+            error: error
+        )
+        
+        #expect(throws: error) {
+            try rule.validate(2)
+        }
+    }
+    
+    @Test func mustBeValidWhenKeyPathValueTransformedNotExistsYet() throws {
+        let game = Game([1, 2, 3, 4])
+        let rule = UniqueRule(
+            game,
+            keyPath: \.values,
+            transform: { $0 + [5, 10, 12] },
+            error: error
+        )
+        
+        try rule.validate(11)
+    }
+    
+    @Test func mustBeInvalidWhenKeyPathValueTransformedAlreadyExists() throws {
+        let game = Game([1, 2, 3, 4])
+        let rule = UniqueRule(
+            game,
+            keyPath: \.values,
+            transform: { $0 + [5, 10, 12] },
+            error: error
+        )
+        
+        #expect(throws: error) {
+            try rule.validate(12)
         }
     }
     
