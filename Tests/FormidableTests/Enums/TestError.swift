13b38be1ed542c161003587e7ecf9579ca3e4ad1
Adriano Souza Costa <adrianosouzacostaios@gmail.com>
2025-01-28 08:09:43 -0300

    Create unit tests for EqualRule.

---
+++ Tests/FormidableTests/Enums/TestError.swift
@@ -0,0 +1,10 @@
+//
+//  TestError.swift
+//  Formidable
+//
+//  Created by Adriano Costa on 28/01/25.
+//
+
+enum TestError: Error, Equatable {
+    case valuesAreNotEqual
+}
--- Tests/FormidableTests/Rules/EqualRuleTests.swift
+++ Tests/FormidableTests/Rules/EqualRuleTests.swift
@@ -2,26 +2,73 @@ import Testing
 
 @testable import Formidable
 
-enum TestError: Error, Equatable {
-    case valuesAreNotEqual
-}
-
 struct EqualRuleTests {
  
+    private let error = TestError.valuesAreNotEqual
+    
     @Test func equalRuleWithStaticValueEqual() throws {
-        let error = TestError.valuesAreNotEqual
         let rule = EqualRule<Any, Int>(10, error: error)
-        
         try rule.validate(10)
     }
     
     @Test func equalRuleWithStaticValueNotEqual() {
-        let error = TestError.valuesAreNotEqual
         let rule = EqualRule<Any, Int>(10, error: error)
-        
-        #expect(throws: TestError.valuesAreNotEqual) {
+
+        #expect(throws: error) {
             try rule.validate(5)
         }
+    }
+    
+    @Test func equalRuleWithKeyPathValueEqual() throws {
+        let person = Person("Orlando")
+        let rule = EqualRule(person, keyPath: \.name, error: error)
+        
+        try rule.validate("Orlando")
+    }
+    
+    @Test func equalRuleWithKeyPathValueNotEqual() throws {
+        let person = Person("Orlando")
+        let rule = EqualRule(person, keyPath: \.name, error: error)
+        
+        #expect(throws: error) {
+            try rule.validate("orlando")
+        }
+    }
+    
+    @Test func equalRuleWithKeyPathValueEqualUsingTransformer() throws {
+        let person = Person("Orlando")
+        let rule = EqualRule(
+            person,
+            keyPath: \.name,
+            transform: { $0 + " Bolotari Costa" },
+            error: error
+        )
+        
+        try rule.validate("Orlando Bolotari Costa")
+    }
+    
+    @Test func equalRuleWithKeyPathValueNotEqualUsingTransformer() throws {
+        let person = Person("Orlando")
+        let rule = EqualRule(
+            person,
+            keyPath: \.name,
+            transform: { $0 + " Bolotari Costa" },
+            error: error
+        )
+        
+        #expect(throws: error) {
+            try rule.validate("orlando costa")
+        }
+    }
+    
+}
+
+fileprivate struct Person {
+    
+    let name: String
+    
+    init(_ name: String) {
+        self.name = name
     }
     
 }
