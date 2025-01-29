60dce55f7fcd2d136256eb5b315c8dfd6597ff92
Adriano Souza Costa <adrianosouzacostaios@gmail.com>
2025-01-29 09:52:49 -0300

    Improve the isHidden view modifier

--- Tests/FormidableTests/FormidableTests.swift
+++ Tests/FormidableTests/FormidableTests.swift
@@ -103,18 +103,6 @@ struct FormidableTests {
     
 }
 
-fileprivate struct Login {
-    
-    let email: String
-    let password: String
-    
-    init(_ email: String, password: String) {
-        self.email = email
-        self.password = password
-    }
-    
-}
-
 fileprivate final class SignInForm: Formidable {
     
     // MARK: Public Variable
@@ -127,17 +115,6 @@ fileprivate final class SignInForm: Formidable {
     init(email: String, password: String) {
         self.emailField = .init(email)
         self.passwordField = .init(password)
-    }
-    
-    // MARK: - Public Methods
-    
-    func submit() throws -> Login {
-        try validate()
-        
-        return .init(
-            emailField.value,
-            password: passwordField.value
-        )
     }
     
 }
