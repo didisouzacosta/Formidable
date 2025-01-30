77700db0ef7758b17ded35d73e9b698720bc3162
Adriano Souza Costa <adrianosouzacostaios@gmail.com>
2025-01-30 07:51:25 -0300

    Improving the accessibility label descrition in to form field container

--- Sources/Formidable/Components/FormFieldContainer.swift
+++ Sources/Formidable/Components/FormFieldContainer.swift
@@ -14,6 +14,7 @@ struct FormFieldContainer<Content: View, Form: FormFieldRepresentable>: View {
     @Binding private var field: Form
     
     private let content: () -> Content
+    private let accessibilityLabel: String
     
     private var messages: [String] {
         (field.showErrors ? field.errors : []).map { $0.localizedDescription }
@@ -23,9 +24,12 @@ struct FormFieldContainer<Content: View, Form: FormFieldRepresentable>: View {
     
     init(
         _ field: Binding<Form>,
+        accessibilityLabel: String,
         @ViewBuilder _ content: @escaping () -> Content
     ) {
         _field = field
+        
+        self.accessibilityLabel = accessibilityLabel
         self.content = content
     }
     
@@ -37,12 +41,12 @@ struct FormFieldContainer<Content: View, Form: FormFieldRepresentable>: View {
                 VStack(alignment: .leading) {
                     ForEach(Array(zip(messages.indices, messages)), id: \.0) { index, message in
                         Text(message)
-                            .accessibilityLabel(message)
                             .id(index)
                     }
                 }
                 .font(.callout)
                 .foregroundStyle(.red)
+                .accessibilityLabel(accessibilityLabel)
             }
         }
         .disabled(field.isDisabled)
--- Sources/Formidable/Modifiers/FieldViewModifier.swift
+++ Sources/Formidable/Modifiers/FieldViewModifier.swift
@@ -9,8 +9,11 @@ import SwiftUI
 
 public extension View {
     
-    func field<Value: Equatable>(_ field: Binding<FormField<Value>>) -> some View {
-        modifier(FieldViewModifier(field))
+    func field<Value: Equatable>(
+        _ field: Binding<FormField<Value>>,
+        accessibilityLabel: String = "Field errors"
+    ) -> some View {
+        modifier(FieldViewModifier(field, accessibilityLabel: accessibilityLabel))
     }
     
 }
@@ -18,9 +21,14 @@ public extension View {
 public struct FieldViewModifier<Value: Equatable>: ViewModifier {
     
     private let field: Binding<FormField<Value>>
+    private let accessibilityLabel: String
     
-    public init(_ field: Binding<FormField<Value>>) {
+    public init(
+        _ field: Binding<FormField<Value>>,
+        accessibilityLabel: String
+    ) {
         self.field = field
+        self.accessibilityLabel = accessibilityLabel
     }
     
     public func body(content: Content) -> some View {
